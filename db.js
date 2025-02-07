afbc8c7fd9244526112276e8b7abb5d3141036cb
tulsyanshubham <tulsyanshubham3639@gmail.com>
2025-02-07 09:50:54 +0530

    update mongo connection URI environment variable

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // const psw = process.env.PSW;
 // const mongoURL = 'mongodb://localhost:27017/Tipp?directConnection=true&readPreference=primary';
-const mongoURL = process.env.MONGOURL;
+const mongoURL = process.env.MONGOURI;
 
 const connectToMongo = async () => {
     await mongoose.connect(mongoURL).then(() => console.log("Connected to MongoDB Successfully"))
